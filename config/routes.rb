716c47315faa3e4303325582a0ecbc45769acfd9
EC2 Default User <ec2-user@ip-172-31-55-189.ec2.internal>
2020-06-30 14:27:14 +0000

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   
-  root "staticpages#home"
-  
-  get 'staticpages/home'
+  get 'users/new'
 
-  get 'staticpages/help'
+  root to:"staticpages#home" 
   
-  get 'staticpages/about'
-  
-  get 'staticpages/contact'
+  get '/home', to:'staticpages#home'
 
+  get '/help', to:'staticpages#help'
+  
+  get '/about', to:'staticpages#about'
+  
+  get '/contact', to:'staticpages#contact'
+  
+  get '/signup',  to: 'users#new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
